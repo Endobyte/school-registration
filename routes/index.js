7482a5a4cacc33da5f9d9f4210351b0d42672ece
Endobyte <endobyte@gmail.com>
2023-02-14 11:29:30 -0800

    Finish CRUD functionality for students

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController')
+const studentController = require('../controllers/studentController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +13,15 @@ router.get('/courses/profile/:id', courseController.viewProfile);
 router.get('/courses/edit/:id', courseController.renderEditForm);
 router.get('/courses/add', courseController.renderAddForm);
 router.get('/courses/delete/:id', courseController.deleteCourse);
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.get('/students/add', studentController.renderAddForm);
+router.get('/students/delete/:id', studentController.deleteStudent);
+
 router.post('/courses/edit/:id', courseController.updateCourse);
 router.post('/courses/add', courseController.addCourse);
+router.post('/students/edit/:id', studentController.updateStudent);
+router.post('/students/add', studentController.addStudent);
+
 module.exports = router;
